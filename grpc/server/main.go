93660a26b187546b47fff358fd207bbdc704d32d
menachem554 <mendyrivkin@gmail.com>
2021-10-20 12:48:47 +0300

    m

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,27 +1,37 @@
 package main
+
 import (
-  "net"
-  "os"
-  mysvccore "github.com/menachem554/go_basic_grpc"
-  mysvcgrpc "github.com/menachem554/go_basic_grpc"
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+
+	mysvccore "github.com/menachem554/go_basic_grpc/core"
+	mysvcgrpc "github.com/menachem554/go_basic_grpc/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
 func main() {
-  // configure our core service
-  userService := mysvccore.NewService()
-  // configure our gRPC service controller
-  userServiceController := NewUserServiceController(userService)
-  // start a gRPC server
-  server := grpc.NewServer()
-  mysvcgrpc.RegisterUserServiceServer(server, userServiceController)
-  reflection.Register(server)
-  con, err := net.Listen("tcp", os.Getenv("GRPC_ADDR"))
-  if err != nil {
-    panic(err)
-  }
-  err = server.Serve(con)
-  if err != nil {
-    panic(err)
-  }
+
+	// configure our core service
+	userService := mysvccore.NewService()
+
+	// configure our gRPC service controller
+	userServiceController := NewUserServiceController(userService)
+
+	// start a gRPC server
+	server := grpc.NewServer()
+	mysvcgrpc.RegisterUserServiceServer(server, userServiceController)
+	reflection.Register(server)
+
+	con, err := net.Listen("tcp", os.Getenv("GRPC_ADDR"))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Starting gRPC user service on %s...\n", con.Addr().String())
+	err = server.Serve(con)
+	if err != nil {
+		panic(err)
+	}
 }
